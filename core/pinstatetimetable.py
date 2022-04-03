9fdd4a82e88712e34973d1d645121220feb5261c
erik <erik@iotech.systems>
2022-04-04 00:11:51 +0200

    midnight fix

--- core/pinstatetimetable.py
+++ core/pinstatetimetable.py
@@ -21,17 +21,16 @@ class pinStateTimetable(object):
          dt_on: datetime.datetime = cmd["on"]
          dt_off: datetime.datetime = cmd["off"]
          # -- check times; return on first match --
-         if dt_on < dt_now < dt_off:
+         if dt_on.time() < dt_now.time() < dt_off.time():
             self.__print_match__(cmdid, dt_on, dt_now, dt_off, "MATCH!")
             self.active_cmd = cmd
             return True
          # -- this should be run over midnight --
-         elif dt_on.hour > dt_off.hour:
-            if (dt_on < dt_now) or ((dt_now.hour < dt_off.hour)
-                  and (dt_now.minute < dt_off.minute)):
-               self.__print_match__(cmdid, dt_on, dt_now, dt_off, "MIDNIGHT MATCH!")
-               self.active_cmd = cmd
-               return True
+         elif (dt_now.time() > dt_on.time() > dt_off.time()) \
+               or (dt_on.time() > dt_off.time() > dt_now.time()):
+            self.__print_match__(cmdid, dt_on, dt_now, dt_off, "MIDNIGHT MATCH!")
+            self.active_cmd = cmd
+            return True
       # -- not found --
       return False
 
