d3e4a455d48f42d4d264226bba2ca89a9e359731
erik <erik@iotech.systems>
2022-04-04 21:25:30 +0200

    fix 33

---
+++ tools/pymb.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import sys, binascii
+import minimalmodbus as mm
+
+
+"""
+   args[0] = this script
+   args[1] = cmd [sndbyts|sndstr]
+   args[2] = ttyUSBx
+   args[3] = modbus adrs
+   args[4] = bytes  00,32,23,xx,xx,xx 
+"""
+
+CMDS = ["SNDBYTES", "SNDSTR"]
+if sys.argv[1].upper() not in CMDS:
+   print(f"\nband cmd: {sys.argv[1]}")
+   exit(0)
+
+CMD = sys.argv[1]
+ttydev = sys.argv[2]
+mbadr = int(sys.argv[3])
+bufstr = sys.argv[4]
+print(f"\n\t--- running {CMD} ---")
+
+buff: bytearray = bytearray()
+for x in sys.argv[3].split(","):
+   n: int = int(x, 16)
+   buff.append(n)
+
+inst: mm.Instrument = mm.Instrument(port=ttydev, slaveaddress=mbadr)
+inst.serial.write(buff)
